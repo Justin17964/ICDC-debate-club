aab0160c8fde3b3d608167d26c98c97606290cc5
Justin17964 <justinsh.qi@gmail.com>
2025-05-05 19:20:29 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4HAgURdf-4kOEziS6W8-KTOZi9_B1aG8",
+  authDomain: "pcdc-2f08e.firebaseapp.com",
+  databaseURL: "https://pcdc-2f08e-default-rtdb.firebaseio.com",
+  projectId: "pcdc-2f08e",
+  storageBucket: "pcdc-2f08e.appspot.com",
+  messagingSenderId: "1024625232109",
+  appId: "1:1024625232109:web:7233436e3217a878cda4e4"
+};
+
+firebase.initializeApp(firebaseConfig);
